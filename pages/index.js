d952d2c73472f3104b7825b95efdb7aa5496b55b
Mikhail Katsman <kry3er@gmail.com>
2023-02-18 23:15:26 +0000

    disabled init scale 1

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
                 />
                 <meta
                     name="viewport"
-                    content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0"
+                    content="width=device-width user-scalable=0"
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
