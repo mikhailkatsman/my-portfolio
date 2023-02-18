83632c37e5a340cd48ed9a1c45eeb61254d63a5e
Mikhail Katsman <kry3er@gmail.com>
2023-02-18 23:21:08 +0000

    disabled user scalability

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,14 @@ export default function Home() {
         <>
             <Head>
                 <title>Mikhail Katsman</title>
+                <meta charset="UTF-8" />
                 <meta
                     name="description"
-                    content="Portfoli of Mikhail Katsman"
+                    content="Portfolio of Mikhail Katsman"
                 />
                 <meta
                     name="viewport"
-                    content="width=device-width user-scalable=0"
+                    content="user-scalable=0"
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
