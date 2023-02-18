33f68f33b1235accb92e734f3a9d63e659fd8012
Mikhail Katsman <kry3er@gmail.com>
2023-02-18 23:08:57 +0000

    disabled dynamic pages 1

--- pages/[projectName]/index.js
+++ pages/[projectName]/index.js
@@ -1,35 +1,35 @@
-import Head from "next/head";
+// import Head from "next/head";
 
-// CLIENT
-export default function Whatever(props) {
-    if (data === null) {
-        return <div>404</div>;
-    }
-    return (
-        <>
-            <Head>
-                <title>{props.name}</title>
-            </Head>{" "}
-            <div>{props.description}</div>
-        </>
-    );
-}
+// // CLIENT
+// export default function Whatever(props) {
+//     if (data === null) {
+//         return <div>404</div>;
+//     }
+//     return (
+//         <>
+//             <Head>
+//                 <title>{props.name}</title>
+//             </Head>{" "}
+//             <div>{props.description}</div>
+//         </>
+//     );
+// }
 
-// SERVER
-export async function getStaticPaths() {
-    let fs = require("fs/promises");
+// // SERVER
+// export async function getStaticPaths() {
+//     let fs = require("fs/promises");
 
-    const projectsDir = await fs.readdir(`${__dirname}/data/projects`);
+//     const projectsDir = await fs.readdir(`${__dirname}/data/projects`);
 
-    return {
-        fallback: false,
-        paths: projectsDir.map((project) => {
-            return { params: { projectName: project.name } };
-        }),
-    };
-}
+//     return {
+//         fallback: false,
+//         paths: projectsDir.map((project) => {
+//             return { params: { projectName: project.name } };
+//         }),
+//     };
+// }
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
 
-    return { props: null };
-}
+//     return { props: null };
+// }
