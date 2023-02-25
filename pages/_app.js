efeadcbdffe65d0b085c1e89622377cde605ce64
Mikhail Katsman <kry3er@gmail.com>
2023-02-25 21:09:35 +0000

    absolute body

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,24 @@ import "@/styles/globals.css";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                <meta charSet="UTF-8" />
-                <meta
-                    name="description"
-                    content="Portfolio of Mikhail Katsman"
-                />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=0.4"
-                />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Component {...pageProps} />
-        </>
-    );
+	return (
+		<>
+			<Head>
+				<meta charSet="UTF-8" />
+				<meta
+					name="description"
+					content="Portfolio of Mikhail Katsman"
+				/>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=0.4"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div className="gradient" />
+			<div className="content">
+				<Component {...pageProps} />
+			</div>
+		</>
+	);
 }
