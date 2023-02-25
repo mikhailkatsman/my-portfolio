e55a9a622e8be842fdba4b5abe9b60da32e83eac
Mikhail Katsman <kry3er@gmail.com>
2023-02-25 20:47:37 +0000

    moved gradient to body::after

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,6 @@ export default function App({ Component, pageProps }) {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="gradient" />
             <Component {...pageProps} />
         </>
     );
