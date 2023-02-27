19eee969c4b80fe152d5073fcfdabbdc0ec7c235
Mikhail Katsman <kry3er@gmail.com>
2023-02-27 13:58:32 +0000

    user scalable yes

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }) {
 				/>
 				<meta
 					name="viewport"
-					content="width=device-width, initial-scale=0.4 user-scalable=no"
+					content="width=device-width, initial-scale=0.4"
 				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
