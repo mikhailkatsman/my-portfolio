2c3b297c13611dc8f34f0807a1c1adf6f153d7b1
Mikhail Katsman <kry3er@gmail.com>
2023-03-07 19:20:22 +0000

    tag redirects and animation adj

--- components/UI/Tag.js
+++ components/UI/Tag.js
@@ -3,11 +3,21 @@ import Image from "next/image";
 import classes from "./Tag.module.css";
 
 export default function Tag(props) {
-	const tagKey = props.key;
+	function handleOnClick(event) {
+		event.preventDefault();
+
+		window.open(props.url, '_ blank');
+		return;
+	}
 
 	return (
-		<li key={props.key}>
-			<a className={classes["tag"]} href={props.url}>
+		<li key={props.name}>
+			<a 
+				className={classes["tag"]} 
+				href={props.url}
+				rel='noopener noreferrer'
+				onClick={handleOnClick}
+			>
 				{props.label}
                 <Image 
 					className={classes['logo']} 
