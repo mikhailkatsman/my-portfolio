f2185ae6792a548ac769f31d542e06e08896c6b3
Mikhail Katsman <kry3er@gmail.com>
2023-03-08 19:55:38 +0000

    media queries scale test

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }) {
 				/>
 				<meta
 					name="viewport"
-					content="width=device-width, initial-scale=0.37 user-scalable=no"
+					content="width=device-width, initial-scale=1"
 				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
