69d9c9e248f3cb00a9f0c8118e7c0a8b5208ad74
Mikhail Katsman <kry3er@gmail.com>
2023-11-15 16:37:24 +0000

    remove next-video trash

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-const { withNextVideo } = require('next-video\process')
-
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
     reactStrictMode: true,
 };
 
-module.exports = withNextVideo(nextConfig, { folder: '/public/videos' });
+module.exports = nextConfig;
